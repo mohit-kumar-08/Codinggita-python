ed661c961625a4bd8184556e4ab7e297a43ed5a9
mohit-kumar-08 <mohit.paramjeet.cg@gmail.com>
2026-09-08 11:37:48 +0530

    Fixed a bug in Q29

--- 08. conditional_statements/Home Assignment/Q29/code.py
+++ 08. conditional_statements/Home Assignment/Q29/code.py
@@ -1,21 +1,22 @@
-is_student = bool(input("Are you a student?: "))
-has_id = bool(input("Do you have id?: "))
-has_ticket = bool(input("Do you have the ticket?: "))
+print("Answer in Yes/No")
+is_student = input("Are you a student?: ")
+has_id = input("Do you have id?: ")
+has_ticket = input("Do you have the ticket?: ")
 
-if is_student == "True":
+if is_student == "Yes":
     is_student = True
-else:
+elif is_student == "No":
     is_student = False
 
-if has_id == "True":
+if has_id == "Yes":
     has_id = True
-else:
+elif has_id == "No":
     has_id = False
 
-if has_student == "True":
-    has_student = True
-else:
-    has_student = False
+if has_ticket == "Yes":
+    has_ticket = True
+elif has_ticket == "No":
+    has_ticket = False
 
 if is_student is True and has_id is True and has_ticket is True:
     print("Allowed")
